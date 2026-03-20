f306e86739802d7f15cce344ac84c6bc4c25e461
Sajin2412 <sajin_iyanavar@zohomail.in>
2026-03-20 11:29:23 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
-window.SUPABASE_URL = "rjctizhtxvlwmutvjxjd";
-window.SUPABASE_ANON_KEY = "sajin-task-management";
+window.SUPABASE_URL = "https://rjctizhtxvlwmutvjxjd.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_0Z0PU-HpJfpcpkL1msZnvQ_WwR2PCio";
+
